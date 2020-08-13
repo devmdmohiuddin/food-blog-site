f8815e1c75f080a7f41cf47262be17ea5b1cfd71
mohiuddin <mohiuddin0240@gmail.com>
2020-08-13 20:44:08 +0600

    food blog site

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function openNav(){
+    document.getElementById('mySidebar').style.width = "50%";
+    document.getElementById('mySidebar').style.transition = ".3s ease";
+}
+function closeNav(){
+    document.getElementById('mySidebar').style.width = "0%";
+    document.getElementById('mySidebar').style.transition = "0s";
+}
